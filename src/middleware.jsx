8bd9fd17b65ef3b1744385a553b41123d77a2b77
SE172303_Duc_Minh <minhnqdse2003@gmail.com>
2024-06-05 02:59:58 +0700

    init stage 2

--- src/middleware.jsx
+++ src/middleware.jsx
@@ -1,6 +1,5 @@
 /* This code snippet is setting up a middleware function in a Next.js application using Next.js API
 routes. Here's a breakdown of what each part is doing: */
-import { getSession } from "next-auth/react";
 import { NextResponse } from "next/server";
 
 const systemRoute = ["/home", "/about", "/mails", "/contacts", "/"];
